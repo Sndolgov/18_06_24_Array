4f98fff8aebd8fb092a22119354085fe19d2ff9d
Sndolgov <Snd251084>
2018-07-31 20:01:42 +0300

    changed HashTableIterator

--- 18_07_29_HashTable/src/telran/util/HashTableIterator.java
+++ 18_07_29_HashTable/src/telran/util/HashTableIterator.java
@@ -2,7 +2,6 @@ package telran.util;
 
 import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
 
 /**
  * Created by Сергей on 29.07.2018.
@@ -10,51 +9,44 @@ import java.util.Set;
 public class HashTableIterator<E> implements Iterator<E> {
     private HashTable<E> table;
     private List<E>[] hashTable;
+    private Iterator<E> currentIterator;
     private int basket = 0;
-    private int indexInList = 0;
-    private int previousIndex;
-    private List<E> currentList;
-    private List<E> previousList;
-    private E current;
+
 
     public HashTableIterator(HashTable<E> table) {
-        this.table=table;
-        hashTable = table.hashTable;
-        if (table.size() > 0)
-            current = getNext();
+        this.table = table;
+        this.hashTable = table.hashTable;
+        if (table.size != 0) {
+            currentIterator = getNextIterator();
+        }
     }
-
 
     @Override
     public boolean hasNext() {
-        return current != null;
+        if (currentIterator!=null && !currentIterator.hasNext())
+            currentIterator = getNextIterator();
+        return currentIterator != null;
     }
 
     @Override
     public E next() {
-        E e = current;
-        previousIndex = indexInList - 1;
-        previousList = currentList;
-        current = getNext();
-        return e;
+            return currentIterator.next();
     }
 
-    private E getNext() {
-        while ((currentList == null || indexInList >= currentList.size()) && basket < hashTable.length) {
-            currentList = hashTable[basket++];
-            indexInList = 0;
+    private Iterator<E> getNextIterator() {
+        List<E> list = null;
+        while (list == null && basket < hashTable.length) {
+            list = hashTable[basket++];
+            if (list != null && list.size() < 1)
+                list = null;
         }
-        if (currentList != null && indexInList < currentList.size()) {
-            return currentList.get(indexInList++);
-        }
-        return null;
+        return list!=null?list.iterator():null;
     }
 
     @Override
     public void remove() {
-        previousList.remove(previousIndex);
+        currentIterator.remove();
         table.size--;
-        if (previousList == currentList)
-            indexInList--;
     }
+
 }
